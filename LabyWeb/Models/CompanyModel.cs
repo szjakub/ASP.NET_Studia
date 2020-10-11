8349cbb17167d1bb28e33437681e3916ca4af466
szjakub <jakubszkodny@gmail.com>
2020-10-11 15:04:15 +0200

    Project initiation

---
+++ LabyWeb/Models/CompanyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabyWeb.Models
+{
+    public class CompanyModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsVisible { get; set; }
+    }
+}
